dfcf8835bd075cf0fb652a7186b72e312c8b1453
Fortuna-457 <alejandro.castrocanalejo@gmail.com>
2024-08-09 14:05:33 +0200

    Terminado Projecto Introducción Curso React

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,31 +1,40 @@
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
+
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Durán',
+        initialIsFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo Hernández',
+        initialIsFollowing: false
+    },
+    {
+        userName: 'goncy',
+        name: 'Nico García',
+        initialIsFollowing: true
+    }
+]
 
 export function App() {
     return (
-        // <article
-        //     style={{
-        //         display: 'flex',
-        //         alignItems: 'center',
-        //         color: '#fff'
-        //     }}
-        // >
-        <article className='tw-followCard'>
-            <header className='tw-followCard-header'>
-                <img
-                    className='tw-followCard-avatar'
-                    src="https://unavatar.io/midudev"
-                    alt="El avatar de midudev"
-                />
-                <div className='tw-followCard-info'>
-                    <strong>Miguel Ángel Durán</strong>
-                    <span className='tw-followCard-infoUserName'>@midudev</span>
-                </div>
-            </header>
-            <aside>
-                <button className='tw-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='App'>
+            {
+                users.map(user => {
+                    const { userName, name, initialIsFollowing } = user
+                    return (
+                        <TwitterFollowCard
+                            key={userName} // Identificador único que indica a React qué es qué
+                            userName={userName}
+                            name={name}
+                            initialIsFollowing={initialIsFollowing}
+                        />
+                    )
+                })
+            }
+        </section>
     )
 }
